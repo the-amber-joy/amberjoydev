71e965af746daa38c0429d574f3a93544415f400
Amber Joy <amber.joy@gmail.com>
2016-03-18 13:24:08 -0500

    express connection correction

--- server/server.js
+++ server/server.js
@@ -2,8 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static('../public'));
-
+app.use(express.static('server/public/'));
 
 app.get('/', function(request, response){
     response.sendFile(path.join(__dirname, '../server/public/views/index.html'));
