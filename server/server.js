9deabcef399b442a42914c29dd85db99c912506b
Amber Joy <amber.joy@gmail.com>
2016-03-18 13:08:15 -0500

    walking skeleton

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static('../public'));
+
+
+app.get('/', function(request, response){
+    response.sendFile(path.join(__dirname, '../server/public/views/index.html'));
+});
+
+var server = app.listen(process.env.PORT || 3000, function(){
+    var port = server.address().port;
+    console.log('Listening on Port:', port)
+});
