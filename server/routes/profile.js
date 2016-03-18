8f592c17cc3a57c1c86ea88a7119697bbfe35458
Amber Joy <amber.joy@gmail.com>
2016-03-18 16:37:54 -0500

    setting it to static html pages

--- server/routes/profile.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var path = require('path');
-var router = express.Router();
-
-router.get('/', function(request, response){
-    response.sendFile(path.join(__dirname, '../public/views/profile.html'));
-});
-
-module.exports = router;
