6661e82d3ebe4423d6da9955755f3f15d842950c
Amber Joy <amber.joy@gmail.com>
2016-03-21 13:15:38 -0500

    clean up formatting

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,28 +3,22 @@ $(document).ready(function() {
         $(this).addClass('animated pulse').siblings().removeClass('animated pulse');
         $(this).addClass('open').siblings().removeClass('open');
     });
-    //
-    //$('.nav-links')
-    //    .on('mouseover', 'li', function() {
-    //    $(this).addClass('animated infinite pulse');
-    //})
-    //    .on('mouseout', 'li', function() {
-    //    $(this).removeClass('animated infinite pulse');
-    //});
 
     $('a[href^="#"]').on('click', function(e) {
-
-        // prevent default anchor click behavior
         e.preventDefault();
-
-        // store hash
         var hash = this.hash;
-
-        // display
         $(hash).siblings().hide();
         $(hash).show().addClass('animated fadeIn').siblings().removeClass('animated fadeIn');
-
     });
 
-});
 
+    $('.nav-links').on('mouseover', 'li', function() {
+            var thisClass = $(this).attr('class');
+            $(this).parent().parent().find('#'+thisClass).show().siblings().hide();
+        })
+        .on('mouseout', 'li', function() {
+        var thisClass = $(this).attr('class');
+            $(this).parent().parent().find('#contact').show();
+            $(this).parent().parent().find('#'+thisClass).hide();
+    })
+});
