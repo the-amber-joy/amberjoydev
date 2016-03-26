775c3e2c768c961b318000e903d233527015bc29
the-amber-joy <amber.joy@gmail.com>
2016-03-26 16:54:55 -0500

    add statcounter, style changes

---
+++ assets/vendors/statcounter.js
@@ -0,0 +1,8 @@
+var sc_project=10886720;
+var sc_invisible=1;
+var sc_security="f8c11854";
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<script type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
